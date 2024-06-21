6e8e1afe91ecfa71d8f8e1bd83480e5682fb82aa
Shane <shanejen56@gmail.com>
2024-06-21 16:28:17 -0700

    basic functionality (adding and displaying) for ONE project now works

--- main.js
+++ main.js
@@ -1,8 +1,52 @@
 import { project } from './project.js';
+import { createTodo } from './createTodo.js';
 
-const project_test = project('one');
-project_test.addTodo('test titlefdsa', 'test description', 2);
-project_test.addTodo('test titlefdas', 'test description', 2);
-project_test.addTodo('test title', 'test description', 2);
-project_test.deleteTodo('test title');
-console.log(project_test.getTodos());
+const container = document.getElementById('container');
+const nameFromInput = document.getElementById('todo-name');
+const listContent = document.getElementById('list-content');
+
+const testButton = document.getElementById('create-todo-button');
+const testProjectButton = document.getElementById('generic-button');
+
+const project_generic = project('one');
+project_generic.addTodo('test titlefdsa', 'test description', 2);
+project_generic.addTodo('test titlefdas', 'test description', 2);
+project_generic.addTodo('test title', 'test description', 2);
+project_generic.deleteTodo('test title');
+
+let currentProject = project_generic;
+displayProject(currentProject);
+
+
+function createTodoCard(projectInstance) {
+    const newTodoItem = projectInstance.addTodo(nameFromInput.value, 'test', 2);
+    const newDiv = document.createElement('div');
+    newDiv.className = 'new-todo';
+    newDiv.textContent = newTodoItem.title;
+
+    listContent.appendChild(newDiv);
+    console.log(project_generic.getTodos());
+
+}
+
+function displayProject(projectInstance) {
+    const todos = projectInstance.getTodos();
+    listContent.innerHTML = ""; // clear previous project
+    for (const todo of todos) {  // display each todo
+        const newDiv = document.createElement('div');
+        newDiv.className = 'new-todo';
+        newDiv.textContent = todo.title;
+        listContent.appendChild(newDiv);
+
+    }
+}
+
+testButton.addEventListener("click", function() {
+    createTodoCard(project_generic);
+    
+});
+
+testProjectButton.addEventListener("click", function() {
+    displayProject(currentProject);
+})
+
